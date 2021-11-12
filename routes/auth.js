6e5f8ebd1e89c5a9b54010bdde43cac277c2bace
Alex Fourmy <alexfourmy@gmail.com>
2021-11-12 14:31:51 +0100

    Implemented user login feature

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 
-// Register
+// REGISTER
 router.post('/register', async (req, res) => {
   try {
     // Generate a hashed password
@@ -20,7 +20,24 @@ router.post('/register', async (req, res) => {
     const user = await newUser.save()
     res.status(200).json(user)
   } catch (err) {
-    console.log(err)
+    res.status(500).json(err)
+  }
+})
+
+// LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    // Retrieve email user
+    const user = await User.findOne({ email: req.body.email })
+    !user && res.status(400).json('User not found!')
+
+    // Retrieve password
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json('Incorrect password!')
+
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json(err)
   }
 })
 
