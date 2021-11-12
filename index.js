88faa96b8786f31e42c7fa6f786b01e8da0d0c7a
Alex Fourmy <alexfourmy@gmail.com>
2021-11-12 14:12:27 +0100

    Connected Express to the app, configured Dotenv, MongoDb and Middlewares

---
+++ index.js
@@ -0,0 +1,37 @@
+// Imports
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
+
+// Connecting App to Express
+const app = express()
+
+// DotEnv config
+dotenv.config()
+
+// MongoDB connection config
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('Connected to MongoDB')
+  }
+)
+
+// Middleware config
+app.use(express.json())
+app.use(helmet())
+app.use(morgan('common'))
+
+//Running our App
+app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
+
+// Express listener
+app.listen(8800, () => {
+  console.log('Backend server is running!')
+})
